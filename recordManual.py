6ab71773d24aa02fe54e8b979416a79fdf2d5782
mea <mahmuodimad@gmail.com>
2026-06-17 01:52:37 -0700

    ran reacher

--- recordManual.py
+++ recordManual.py
@@ -1,16 +1,15 @@
-import gym
-from gym.wrappers import RecordVideo
+import gymnasium as gym
+from gymnasium.wrappers import RecordVideo
 from stable_baselines3 import DDPG
 
 model = DDPG.load("./model.zip")
-env = gym.make("Reacher-v2")
-env = RecordVideo(env, "videos/", episode_trigger=lambda x: True)  # record every episode
+env = gym.make("Reacher-v4", render_mode="rgb_array", max_episode_steps=300)  # longer episodes
+env = RecordVideo(env, "videos/", episode_trigger=lambda x: True)
 
 obs, _ = env.reset()
-for _ in range(200):  # this will run ~4 episodes (200 steps)
+for _ in range(300):
     action, _ = model.predict(obs, deterministic=True)
     obs, reward, terminated, truncated, _ = env.step(action)
     if terminated or truncated:
         obs, _ = env.reset()
 env.close()
-print("Video(s) saved in videos/ folder")
